1cd1478aac61ff78a57bf97b06f733dd4cd241cb
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-07-03 15:21:19 -0500

    redid user routes and some user.js for login and password

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,79 +7,81 @@ const User = require("../../models/user");
 
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  User.findAll({
-    where: {
-      email: req.params.email,
-    },
-  }).then((dbUserData) => {
-    res.json(dbUserData);
-  });
-});
-
-// router.get('/', (req, res) => {
-//     Post.findAll({
-//         attributes: [
-//             'email',
-//             'password'
-//         ],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['userEmail', 'userPsw']
-//             }
-//         ]
-//     }).then(dbPostData => {
-//         const posts = dbPostData.map(post => post.get({ plain: true }));
-//         res.render('homepage', {
-//             posts,
-//             loggedIn: req.session.loggedIn
-//         });
-//     }).catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
-
-//GET session login
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  res.render("login");
-});
-//GET signup
-
-router.get("/login-signup", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/homepage");
-    return;
-  }
-  res.render("signup");
-});
-
-// GET post by id
-router.post("/:login", (req, res) => {
+router.post("/login", (req, res) => {
   User.findOne({
     where: {
       email: req.body.email,
     },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put("/:id", (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
   })
     .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this email" });
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
-      res.render("/homepage", {
-        loggedIn: req.session.loggedIn,
-      });
+      res.json(dbUserData);
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-module.exports = router;
 
-//Encryption code for the database
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection.js");
 
 class User extends Model {}
@@ -18,6 +19,23 @@ User.init(
     userPsw: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
     },
   },
   {
